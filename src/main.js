c4eea133a6cdc16c0a980e43da05253f83bbc7bf
Potat0x <Potat0x@users.noreply.github.com>
2020-09-05 21:28:07 +0200

    Adding publication date to video title

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-function x() {
-    console.log("page ready");
-    alert("page ready");
+function getVideoTitle() {
+    return document.title.substring(0, document.title.length - " - YouTube".length);
 }
 
-function a() {
-    setTimeout(function () { x(); }, 2000);
+function getTitleElement() {
+    return document.querySelector("h1.ytd-video-primary-info-renderer yt-formatted-string");
 }
 
-document.onload = a();
+function getPublicationDateElement() {
+    const a = document.querySelector("div#date yt-formatted-string.ytd-video-primary-info-renderer");
+    return a;
+}
+
+function appendPublicationDateToTitle() {
+    const titleElement = getTitleElement();
+    const publicationDate = getPublicationDateElement().innerText;
+    titleElement.innerHTML = getVideoTitle() + "&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;" + publicationDate;
+}
+
+setTimeout(appendPublicationDateToTitle, 2000);
